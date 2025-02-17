daad0f7d859e723a2887e296a0a72f782f75b546
Admin <nguy1104@msu.edu>
2025-02-16 23:50:19 -0500

    finished skeleton

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
-
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
+  // Copy over images, CSS, etc. so they are available in _site
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  return {
+    dir: {
+      input: ".",         // Our main input directory is the project root
+      includes: "_includes", // Where partials/layouts live
+      output: "_site"     // Default output folder
+    },
+    // Use Liquid by default
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "liquid",
+    dataTemplateEngine: "liquid"
+  };
 };
--- about.md
+++ about.md
@@ -3,5 +3,6 @@ layout: layout.liquid
 title: About
 ---
 
-# About the 11ty **Boilerplate**
-<img class="about" alt="pine" src="/images/pine.png" width="50" />
+# About Polaroid Cloud
+
+We help you sync your photos to your favorite cloud service, while still enjoying the instant print magic of Polaroid. Perfect for sharing with friends and family.
